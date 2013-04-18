4c981ea0e9bfdbc82c363214093a34367846f5bd
broller <broller@taylor.edu>
2013-04-18 12:38:19 -0400

    Answer Block and Basic text obj initial commit.

---
+++ Verse-shoot/src/org/example/verse_shoot/textObj.java
@@ -0,0 +1,15 @@
+package org.example.verse_shoot;
+
+public class textObj {
+	String word;
+	
+	public void set(String setText)
+	{
+		word = setText;
+	}
+	
+	public String get(){
+		return word;
+	}
+}
+
