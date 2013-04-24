497d6211bd5f2c1f7f88eb61473f1a4097a2fb9f
Josh Hapner <joshua_hapner1@taylor.edu>
2013-04-24 19:15:12 -0400

    versechooser initial commit

---
+++ Verse-shoot/src/org/example/verse_shoot/VerseChooser.java
@@ -0,0 +1,12 @@
+package org.example.verse_shoot;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VerseChooser extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.versechooser);
+	}
+}
