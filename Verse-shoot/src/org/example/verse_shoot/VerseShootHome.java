d165a3d0dd480bc03af289f09f2aa247ba88521e
andrewketchum <andrewketchum@10.119.201.75>
2013-04-25 12:30:19 -0400

    thurs 425 1230pm

--- Verse-shoot/src/org/example/verse_shoot/VerseShootHome.java
+++ Verse-shoot/src/org/example/verse_shoot/VerseShootHome.java
@@ -1,5 +1,11 @@
 package org.example.verse_shoot;
 
+import java.lang.reflect.Array;
+import static java.util.Arrays.asList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -14,14 +20,17 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 public class VerseShootHome extends Activity implements OnClickListener {
-
-	//Button leftButton, rightButton, beamL, beamR;
+	
 	ImageView leftButton, rightButton,lightBeam;
 	Button beamL, beamR;
 	TextView lt, mt, rt,la,ra;
+	ArrayList<String> randWords;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	String[] wordList = {"John","Mary","Jesus","God","Lord",
+    			"Christ","Matthew","Judas","Peter","Paul"};
+    	randWords = new ArrayList<String>(Arrays.asList(wordList));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verse_shoot_home);
         //leftButton = (Button) findViewById(R.id.leftButton);
@@ -37,65 +46,33 @@ public class VerseShootHome extends Activity implements OnClickListener {
         lt = (TextView) findViewById(R.id.w1);
         mt = (TextView) findViewById(R.id.w2);
         rt = (TextView) findViewById(R.id.w3);
+        Random rand = new Random();
+        int lRand = rand.nextInt(randWords.size() - 1);
+        lt.setText(randWords.get(lRand));
+        randWords.remove(lRand);
+        randWords.trimToSize(); 
+        int mRand = rand.nextInt(randWords.size() - 1);
+        mt.setText(randWords.get(mRand));
+        randWords.remove(mRand);
+        randWords.trimToSize();
+        int rRand = rand.nextInt(randWords.size() - 1);
+        rt.setText(randWords.get(rRand));
+        randWords.remove(rRand);
+        randWords.trimToSize();
+        switch(rand.nextInt(3)) {
+        	case 0:
+        		lt.setText("FROMVERSE");
+        		break;
+        	case 1:
+        		mt.setText("FROMVERSE");
+        		break;
+        	case 2:
+        		rt.setText("FROMVERSE");
+        		break;
+        }
         la = (TextView) findViewById(R.id.leftAnim);
         ra = (TextView) findViewById(R.id.rightAnim);
-//        leftButton.setOnClickListener(new OnClickListener ()
-//        {
-//        	public void onClick(View v)
-//        	{
-//        		CharSequence left = lt.getText();
-//        		lt.setText(mt.getText());
-//        		mt.setText(rt.getText());
-//        		rt.setText(left);
-//        		Toast msg = Toast.makeText(getBaseContext(), "Left Button",Toast.LENGTH_SHORT);
-//        		msg.show();
-//        	}
-//        });
-//        rightButton.setOnClickListener(new OnClickListener ()
-//        {
-//        	public void onClick(View v)
-//        	{
-//        		CharSequence right = rt.getText();
-//        		rt.setText(mt.getText());
-//        		mt.setText(lt.getText());
-//        		lt.setText(right);
-//        		Toast msg = Toast.makeText(getBaseContext(), "Right Button", Toast.LENGTH_SHORT);
-//        		msg.show();
-//        	}
-//        });
-//        beamL.setOnClickListener(new OnClickListener ()
-//        {
-//        	public void onClick(View v)
-//        	{
-//        		
-//        		Toast msg = Toast.makeText(getBaseContext(), "BEAM!!", Toast.LENGTH_SHORT);
-//        		msg.show();
-//        	}
-//        });
-//        beamR.setOnClickListener(new OnClickListener ()
-//        {
-//        	public void onClick(View v)
-//        	{
-//        		Toast msg = Toast.makeText(getBaseContext(), "BEAM!!", Toast.LENGTH_SHORT);
-//        		msg.show();
-//        	}
-//        });
     }
-
- /*   private void addListenerOnButton() {
-    	leftButton = (Button) findViewById(R.id.leftButton);	
-    	instead of button ImageView 
-		leftButton.setOnClickListener(new OnClickListener() {
- 
-			@Override
-			public void onClick(View arg0) {
- 
-//				Toast.makeText(VerseShootHome.this,
-//					"LeftButton is clicked!",
-//					Toast.LENGTH_SHORT).show();
-			}
-		});		
-	}*/
     
     @Override
     public void onClick (View v) {
