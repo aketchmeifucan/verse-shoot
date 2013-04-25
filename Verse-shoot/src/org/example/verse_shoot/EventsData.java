c9c2680e5c9051c97bc2a98141b040382edb6ccb
Josh Hapner <joshua_hapner1@taylor.edu>
2013-04-24 20:49:53 -0400

    first test

---
+++ Verse-shoot/src/org/example/verse_shoot/EventsData.java
@@ -0,0 +1,37 @@
+/***
+ * Excerpted from "Hello, Android",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/eband3 for more book information.
+***/
+package org.example.verse_shoot;
+
+import static android.provider.BaseColumns._ID;
+import static org.example.verse_shoot.Constants.TABLE_NAME;
+import static org.example.verse_shoot.Constants.VERSE;
+import static org.example.verse_shoot.Constants.VERSE_TEXT;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class EventsData extends SQLiteOpenHelper {
+   private static final String DATABASE_NAME = "test.db";
+   private static final int DATABASE_VERSION = 1;
+
+   /** Create a helper object for the Events database */
+   public EventsData(Context ctx) {
+      super(ctx, DATABASE_NAME, null, DATABASE_VERSION);
+   }
+
+   @Override
+   public void onCreate(SQLiteDatabase db) {
+   }
+
+   @Override
+   public void onUpgrade(SQLiteDatabase db, int oldVersion,
+         int newVersion) {
+   }
+
+}
