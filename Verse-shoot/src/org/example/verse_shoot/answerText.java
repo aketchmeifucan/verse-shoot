54f66bf19d20d803813f941b0418aa022300541c
andrewketchum <andrewketchum@10.119.201.75>
2013-05-02 12:17:07 -0400

    switched to black and modified bens code

--- Verse-shoot/src/org/example/verse_shoot/answerText.java
+++ Verse-shoot/src/org/example/verse_shoot/answerText.java
@@ -6,9 +6,10 @@ import java.util.Random;
 import org.example.verse_shoot.textObj;
 
 public class answerText {
-	public textObj[] textStrings;// The array of text containing the verse.
+	//public textObj[] textStrings = new textObj[500];// The array of text containing the verse.
+	public String[] textStrings = new String[500];
 	public int iterator = 0;		// Earliest unfilled blank.
-	public int[] filledStrings; // Ordered list. Index of filled string. Draw should use to determine what strings answered/filled in.
+	public List<Integer> filledStrings; // Ordered list. Index of filled string. Draw should use to determine what strings answered/filled in.
 	
 	
 	
@@ -22,9 +23,9 @@ public class answerText {
 		// Split the full verse into temp, iterate temp and put into textObjs.
 		String[] temp;
 		temp = verse.split(" ");
-		textStrings[0].set(ref);	//Set the reference as the first word.
+		textStrings[0] = (ref);	//Set the reference as the first word.
 		for(int i = 0; i<temp.length;i++){
-			textStrings[i+1].set(temp[i]); //i+1 to dodge the ref word.
+			textStrings[i+1] = (temp[i]); //i+1 to dodge the ref word.
 		}		
 		return true;
 	}
@@ -32,15 +33,14 @@ public class answerText {
 	//Fill the word and move iterator to next unfilled word.
 	//Return iterator position
 	public int fillWord(){
-		List a = Arrays.asList(filledStrings);
-		a.add(iterator);
+		filledStrings.add(iterator);
 		return nextWord();
 	}
 	
 	//Move the iterator to the next unfilled word
 	//Return new iterator position
 	public int nextWord(){
-		while(Arrays.asList(filledStrings).contains(iterator))	// Can be made more efficient if changed to use list iterator.
+		while(filledStrings.contains(iterator))	// Can be made more efficient if changed to use list iterator.
 		{
 			iterator++;
 		}		
@@ -49,22 +49,24 @@ public class answerText {
 	
 	//Randomly select filled words. (fill half of the words)
 	private void randomizeBlanks(){
-		int numToFill = (textStrings.length/2);	// Half of the words
+		//int numToFill = (textStrings.length/2);	// Half of the words
+		int numToFill = 5;
 		for(int i = 0; i < numToFill; i++)		// Iterate each blank and fill it with a randomly selected index that does not already exist in filled strings
 		{
 			Random rand = new Random(); 
-			int[] x = new int[1]; 
-			x[0] = rand.nextInt(textStrings.length); 
-			if(Arrays.asList(filledStrings).contains(x))
+			int x = rand.nextInt(textStrings.length - 1);
+			//int[] x = new int[1]; 
+			//x[0] = rand.nextInt(textStrings.length); 
+			if(filledStrings.contains(x))
 				i--;
 			else
-				Arrays.asList(filledStrings).add(x);
+				filledStrings.add(x);
 		}
 	}
 	
 	//Function to return the current word
 	public String currentWord(){
-		return textStrings[iterator].get();
+		return textStrings[iterator];
 	}
 	
 	// Tells if we are done with this verse ( be sure to check if last word is not blank)
@@ -79,8 +81,8 @@ public class answerText {
 	private String genString(){
 		String ansVerse = "";
 		for(int i = 0; i <textStrings.length; i++){
-			if(Arrays.asList(filledStrings).contains(i)){	// Filled in word
-				ansVerse = ansVerse + textStrings[i].get();
+			if(filledStrings.contains(i)){	// Filled in word
+				ansVerse = ansVerse + textStrings[i];
 			} else{
 				if(i == iterator){	// Highlighted Blank
 					ansVerse = ansVerse + " << _ >> ";
@@ -89,8 +91,7 @@ public class answerText {
 				}				
 			}
 			
-		}
-		
+		}	
 		return ansVerse;
 	}
 	
